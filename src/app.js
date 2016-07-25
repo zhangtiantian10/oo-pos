72e59bb284aa9ee01e3976d31c4529ed3dadc9e0
zhangtiantian10 <975571814@qq.com>
2016-07-25 19:35:37 +0800

    move loadAllItems funtion to Item.all method

--- src/app.js
+++ src/app.js
@@ -1,14 +1,13 @@
 const fixture = require('./fixtures');
-const loadAllItems = fixture.loadAllItems;
 const loadPromotions = fixture.loadPromotions;
 const CartItem = require('./models/cart-item');
 const ReceiptItem = require('./models/receipt-item');
 const Receipt = require('./models/receipt');
+const Item = require('./models/item');
 
 function printReceipt(tags) {
 
-  const allItems = loadAllItems();
-  const cartItems = buildCartItems(tags, allItems);
+  const cartItems = buildCartItems(tags, Item.all());
 
   const allPromotions = loadPromotions();
   const receiptItems = buildReceiptItems(cartItems, allPromotions);
